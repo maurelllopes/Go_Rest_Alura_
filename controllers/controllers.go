2015ad1a862eb918075cf5f6ba53baa2e8e05e05
maurelllopes <maurelllopes@github.com>
2022-04-10 22:29:55 -0300

    Chamando personalidades com a rota

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"Go_Rest_Alura_/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"Go_Rest_Alura_/models"
 	"Go_Rest_Alura_/routes"
 	"fmt"
 )
 
 func main() {
+
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
 	fmt.Println("Iniciando Servidor Rest com Go")
 	routes.HandleRequest()
 }
