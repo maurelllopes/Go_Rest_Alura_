0f98c8f904617e175a13e2123ecccd0334f40c4b
maurelllopes <maurelllopes@github.com>
2022-04-10 21:52:00 -0300

    Pacotes Routes e Controllers

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
+	"Go_Rest_Alura_/routes"
 	"fmt"
-	"log"
-	"net/http"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
 	fmt.Println("Iniciando Servidor Rest com Go")
-	HandleRequest()
+	routes.HandleRequest()
 }
