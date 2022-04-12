44f9c372c6b9bbb612cb1411219d2832510f0ffa
maurelllopes <maurelllopes@github.com>
2022-04-11 22:58:06 -0300

    Criando Get, Post, Delete, Put

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,10 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
